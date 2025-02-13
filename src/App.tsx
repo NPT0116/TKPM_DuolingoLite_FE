20d16d9c4c05a7e85e7fbfd2f8f3063a1ac056e6
mJota666 <micode2744@gmail.com>
2025-02-13 15:27:53 +0700

    create PATH file

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./views/pages/LoginPage";
 import RegisterPage from "./views/pages/RegisterPage";
 import GuestPage from "./views/pages/GuestPage";
+import { PATH } from "./routes/routes";
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<GuestPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route path={PATH.GUEST.index} element={<GuestPage />} />
+      <Route path={PATH.LOGIN.index} element={<LoginPage />} />
+      <Route path={PATH.REGISTER.index} element={<RegisterPage />} />
     </Routes>
   );
 };
---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+export const PATH = {
+    LOGIN: {
+        index: "/login"
+    },
+    REGISTER: {
+        index: "/register"
+    },
+    GUEST: {
+        index: "/"
+    }
+}
