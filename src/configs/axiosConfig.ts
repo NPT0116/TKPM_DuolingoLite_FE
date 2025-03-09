1c0ffac1b2c400a230ae3a406d1a3ef5c90fdfc8
mJota666 <micode2744@gmail.com>
2025-03-09 15:10:55 +0700

    handle invalid input filed in login and register

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -1,5 +1,31 @@
-// import axios from "axios";
+import axios from "axios";1
 
-// const api = axios.create({
-//     baseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:8082/api"
-// })
+// Fix the logical OR placement
+const url = import.meta.env.VITE_PORT ? `http://localhost:${import.meta.env.VITE_PORT}/api` : "http://localhost:8082/api"
+const api = axios.create({
+    baseURL:  url
+});
+const token = localStorage.getItem("authToken");
+if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer: ${token}`
+}
+axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem("authToken");
+        window.location.href = "/login";
+      }
+      return Promise.reject(error);
+    }
+  );
+  
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
+export default api;
