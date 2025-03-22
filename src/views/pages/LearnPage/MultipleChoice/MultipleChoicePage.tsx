53daf46b76d8119eb19186d263d94bc28be9b65c
huy37204 <huy37204@gmail.com>
2025-03-22 11:24:49 +0700

    DUOLITE-31 shuffle question and append false answer into question list

--- src/views/pages/LearnPage/MultipleChoice/MultipleChoicePage.tsx
+++ src/views/pages/LearnPage/MultipleChoice/MultipleChoicePage.tsx
@@ -3,12 +3,15 @@ import { IMultipleChoiceQuestion } from "../../../../interfaces/Questions/IMulti
 import { useEffect, useState } from "react";
 import AnswerSectionImage from "../../../components/Learning/MultipleChoice/AnswerSection/AnswerSectionImage";
 import AnswerSection from "../../../components/Learning/MultipleChoice/AnswerSection/AnswerSection";
+import retry_img from "../../../../assets/imgs/retry_img.png";
 
 interface MultipleChoiceProps {
   data: IMultipleChoiceQuestion;
   setIsButtonActive: React.Dispatch<React.SetStateAction<boolean>>;
   setIsButtonCorrect: React.Dispatch<React.SetStateAction<boolean>>;
   isSubmit: boolean;
+  isQuestionRetry: boolean;
+  state: number;
 }
 
 const MultipleChoicePage: React.FC<MultipleChoiceProps> = ({
@@ -16,17 +19,28 @@ const MultipleChoicePage: React.FC<MultipleChoiceProps> = ({
   setIsButtonCorrect,
   data,
   isSubmit,
+  isQuestionRetry,
+  state,
 }) => {
   const hasImageAnswer = data.optionConfigure.image !== false;
   const [isNext, setIsNext] = useState(false);
 
   useEffect(() => {
     setIsNext(true);
-  }, [data.questionId]);
+  }, [state]);
 
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="w-full sm:w-full md:w-[600px] min-w-[600px] max-h-[450px] flex flex-col justify-center  ">
+        {isQuestionRetry && (
+          <div
+            className="flex gap-2 text-[#ffab33] font-bold text-[16px]"
+            style={{ margin: "0 0 20px 10px" }}
+          >
+            <img src={retry_img} alt="" />
+            <span>LỖI SAI TRƯỚC ĐÂY</span>
+          </div>
+        )}
         {/* Instruction */}
         <Instruction instruction={data.instruction} />
 
