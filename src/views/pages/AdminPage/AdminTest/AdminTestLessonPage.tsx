30e68026af72e74c34b4477d75ef23667bcf892a
mJota666 <micode2744@gmail.com>
2025-04-06 14:09:17 +0700

    DUOLITE71 merge DUOLITE68

--- src/views/pages/AdminPage/AdminTest/AdminTestLessonPage.tsx
+++ src/views/pages/AdminPage/AdminTest/AdminTestLessonPage.tsx
@@ -6,10 +6,11 @@ import TestContinueButton from "../../../components/Button/TestContinueButton";
 import FooterStatus from "../../../components/FooterBar/FooterStatus";
 import correctSound from "../../../../assets/sounds/duo_correct_sound.mp4";
 import incorrectSound from "../../../../assets/sounds/duo_incorrect_sound.mp4";
+import { AudioProvider } from "../../../components/LearnPage/Audio/AudioProvider";
 
 const AdminTestLessonPage = () => {
   const [question, setQuestion] = useState<IQuestion | null>(null);
-  const lessonId = "00922209-1ba2-4f81-a1be-2a84a0e24663";
+  const lessonId = "31dc76be-c835-473c-9def-1f754b9732e5";
   const [isButtonActivate, setIsButtonActive] = useState(false);
   const [isButtonCorrect, setIsButtonCorrect] = useState(false);
   const [isNext, setIsNext] = useState(false);
@@ -57,28 +58,30 @@ const AdminTestLessonPage = () => {
   }, [isSubmit, isNext, isRetry]);
 
   return (
-    <div className="bg-[#131F23] h-full overflow-auto">
-      <div className="h-[70vh] w-[100vw]">{handleLesson(question!)}</div>
-      <div
-        className="bg-[#131F23] border-[#37464F] border-t-2 h-[20vh] w-[100vw] relative"
-        // style={{ background: isNext ? "#202F36" : "" }}
-      >
-        {isNext && isButtonCorrect && <FooterStatus type={0} />}
-        {isNext && !isButtonCorrect && <FooterStatus type={1} />}
-        {!isNext && isRetry && <FooterStatus type={2} />}
-        <TestContinueButton
-          isSubmit={isSubmit}
-          setIsSubmit={setIsSubmit}
-          setIsNext={setIsNext}
-          isButtonActivate={isButtonActivate}
-          isButtonCorrect={isButtonCorrect}
-          hoverColor="4156FF"
-          paddingWidth={80}
-          positionRight={250}
-          type="admin-learn" //Nếu type learn thì navigate về home, nếu review navigate về review
-        />
+    <AudioProvider>
+      <div className="bg-[#131F23] h-full overflow-auto">
+        <div className="h-[70vh] w-[100vw]">{handleLesson(question!)}</div>
+        <div
+          className="bg-[#131F23] border-[#37464F] border-t-2 h-[20vh] w-[100vw] relative"
+          // style={{ background: isNext ? "#202F36" : "" }}
+        >
+          {isNext && isButtonCorrect && <FooterStatus type={0} />}
+          {isNext && !isButtonCorrect && <FooterStatus type={1} />}
+          {!isNext && isRetry && <FooterStatus type={2} />}
+          <TestContinueButton
+            isSubmit={isSubmit}
+            setIsSubmit={setIsSubmit}
+            setIsNext={setIsNext}
+            isButtonActivate={isButtonActivate}
+            isButtonCorrect={isButtonCorrect}
+            hoverColor="4156FF"
+            paddingWidth={80}
+            positionRight={250}
+            type="admin-learn" //Nếu type learn thì navigate về home, nếu review navigate về review
+          />
+        </div>
       </div>
-    </div>
+    </AudioProvider>
   );
 };
 
